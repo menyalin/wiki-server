d74730a31d14efb2882c052c04afd5c4a42913c9
Menyalin Alexey <menyalin@gmail.com>
2022-07-10 19:49:30 +0300

    add class validator

--- src/article/schemas/group.schema.ts
+++ src/article/schemas/group.schema.ts
@@ -1,16 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { MaxLength, IsInt, Min, Max, IsString } from 'class-validator'
+
 
 export type GroupDocument = Group & Document;
 
 @Schema({collection: 'groups'})
 export class Group {
   _id: string
-
+  @IsString()
+  @MaxLength(25, {message: 'Title is too long. Maximal length is $constraint1 characters, but actual is $value'})
   @Prop({ required: true })
   title: string
 
   @Prop({default: 1000})
+  @IsInt()
+  @Min(0)
+  @Max(1000)
   listIndex: number
 }
 
