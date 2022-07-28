bb0be1032e3489ba709a1e878d9c5118e77155e8
Menyalin Alexey <menyalin@gmail.com>
2022-07-28 12:43:44 +0300

    add log

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 import {ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+  app.enableCors()
   app.setGlobalPrefix('api')
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(parseInt(process.env.PORT));
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(parseInt(process.env.PORT), () => {
+    console.log(`app started on  ${process.env.PORT}`)
+  })
 
 }
-bootstrap();
+bootstrap()
